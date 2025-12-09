05e07f41ad686c169b5148b291021118127c8121
sakinkhan <sakin2044@gmail.com>
2025-12-09 23:26:32 +1100

    tuition APIs added

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
+//firebase admin
+const admin = require("firebase-admin");
+const serviceAccount = require("./etuitionbd-sakinkhan-firebase-adminsdk.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 // middlewares
 app.use(express.json());
 app.use(cors());
+
+const verifyFBToken = async (req, res, next) => {
+  //   console.log("headers in the MIddLeWare", req.headers.authorization);
+
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  try {
+    const idToken = token.split(" ")[1];
+    const decoded = await admin.auth().verifyIdToken(idToken);
+    console.log("decoded in the token", decoded);
+    // req.decoded_email = decoded.email;
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@sakinkhan.slpidbs.mongodb.net/?appName=SakinKhan`;
 
@@ -25,21 +52,24 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // Collections
     const db = client.db("eTuitionBD_db");
     const usersCollection = db.collection("users");
+    const tuitionPostsCollection = db.collection("tuitionPosts");
 
     /*-----------USER ENDPOINTS---------*/
-    // Get all users OR filter by role
+    // GET all users OR filter by role
     app.get("/users", async (req, res) => {
       const role = req.query.role;
+      console.log("HEADERS", req.headers);
       const filter = role ? { role } : {};
 
-      const cursor = await usersCollection.find(filter);
-      const result = cursor.toArray();
+      const cursor = usersCollection.find(filter);
+      const result = await cursor.toArray();
       res.send(result);
     });
 
-    // create a new user
+    // create/POST a new user
     app.post("/users", async (req, res) => {
       const user = req.body;
 
@@ -55,6 +85,182 @@ async function run() {
       }
 
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    /*-----------Tuition Post ENDPOINTS---------*/
+    // GET all tuition posts (latest first)
+    app.get("/tuition-posts", async (req, res) => {
+      try {
+        const query = {};
+        const cursor = tuitionPostsCollection
+          .find(query)
+          .sort({ createdAt: -1 });
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ error: "Failed to fetch tuition posts" });
+      }
+    });
+
+    // GET Single tuition post by id
+    app.get("/tuition-posts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await tuitionPostsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // GET all my tuition posts
+    app.get("/tuition-posts/my-posts", verifyFBToken, async (req, res) => {
+      const fbEmail = req.user?.email;
+      if (!fbEmail) {
+        return res.status(400).send({ error: "Invalid authentication" });
+      }
+
+      const cursor = tuitionPostsCollection
+        .find({ userEmail: fbEmail })
+        .sort({ createdAt: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // POST/ Create a tuition post
+    app.post("/tuition-posts", verifyFBToken, async (req, res) => {
+      const {
+        subject,
+        classLevel,
+        location,
+        budget,
+        schedule,
+        description,
+        contactEmail,
+      } = req.body;
+
+      //get student name
+      const fbEmail = req.user.email;
+      const dbUser = await usersCollection.findOne({ email: fbEmail });
+      const studentName = dbUser?.name || "Unknown";
+
+      console.log("contactEmail + studentName", contactEmail, studentName);
+
+      // Basic validation
+      if (
+        !subject ||
+        !classLevel ||
+        !location ||
+        !budget ||
+        !contactEmail ||
+        !description
+      ) {
+        return res.status(400).send({
+          error: "Missing required fields.",
+          required: [
+            "subject",
+            "classLevel",
+            "location",
+            "budget",
+            "contactEmail",
+            "description",
+          ],
+        });
+      }
+      const newPost = {
+        studentName,
+        subject,
+        classLevel,
+        location,
+        budget: Number(budget),
+        schedule: schedule || "",
+        description,
+        contactEmail,
+        status: "Approved", // TODO make pending once admin done
+        createdAt: new Date(),
+        userEmail: fbEmail,
+      };
+      console.log("NEW POST", newPost);
+
+      const result = await tuitionPostsCollection.insertOne(newPost);
+      res.send(result);
+    });
+
+    // PATCH/edit tuition posts
+    app.patch("/tuition-posts/:id", verifyFBToken, async (req, res) => {
+      const id = req.params.id;
+      const fbEmail = req.user?.email;
+      if (!fbEmail)
+        return res.status(400).send({ error: "Invalid authentication" });
+
+      const query = { _id: new ObjectId(id) };
+      const existingPost = await tuitionPostsCollection.findOne(query);
+      if (!existingPost)
+        return res.status(404).send({ error: "Tuition post not found" });
+
+      if (existingPost.userEmail !== fbEmail)
+        return res.status(403).send({ error: "Forbidden. Not your post." });
+
+      const {
+        subject,
+        classLevel,
+        location,
+        budget,
+        schedule,
+        description,
+        contactEmail,
+      } = req.body;
+
+      const updatedFields = {};
+      if (subject) updatedFields.subject = subject;
+      if (classLevel) updatedFields.classLevel = classLevel;
+      if (location) updatedFields.location = location;
+      if (budget !== undefined && budget !== "")
+        updatedFields.budget = Number(budget);
+      if (schedule !== undefined) updatedFields.schedule = schedule;
+      if (description) updatedFields.description = description;
+      if (contactEmail) updatedFields.contactEmail = contactEmail;
+      updatedFields.updatedAt = new Date();
+
+      const result = await tuitionPostsCollection.updateOne(query, {
+        $set: updatedFields,
+      });
+
+      if (result.matchedCount === 0) {
+        return res.status(404).send({ error: "Tuition post not found" });
+      }
+
+      res.send({ message: "Tuition post updated successfully", updatedFields });
+    });
+
+    // DELETE tuition posts
+    app.delete("/tuition-posts/:id", verifyFBToken, async (req, res) => {
+      const id = req.params.id;
+
+      const fbEmail = req.user?.email;
+      if (!fbEmail) {
+        return res.status(400).send({ error: "Invalid authentication" });
+      }
+
+      // Find the post
+      const existingPost = await tuitionPostsCollection.findOne({
+        _id: new ObjectId(id),
+      });
+
+      if (!existingPost) {
+        return res.status(404).send({ error: "Tuition post not found" });
+      }
+
+      // Ownership check
+      if (existingPost.userEmail !== fbEmail) {
+        return res
+          .status(403)
+          .send({ error: "Forbidden. You cannot delete this post." });
+      }
+
+      const result = await tuitionPostsCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+
       res.send(result);
     });
 
